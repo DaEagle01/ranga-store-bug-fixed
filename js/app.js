8d65d6f129e575665b464f74d79ece3973804c63
DaEagle01 <fbinkarim01@gmail.com>
2021-09-13 03:34:11 +0600

    enhance

--- js/app.js
+++ js/app.js
@@ -20,6 +20,7 @@ const showProducts = (products) => {
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
+      <h4>${product.rating.rate} <i class="fas fa-star star"></i>  &nbsp &nbsp &nbsp &nbsp  <i class="fas fa-user"></i> ${product.rating.count}</h4>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
